6adbf22d7da4a31bbb7772e510d0a8c090157ab6
MahmoudSfn <mahmoudsouayfan@gmail.com>
2020-03-25 08:54:54 +0100

    qwerty

--- mainApp.py
+++
@@ -1,9 +0,0 @@
-from instapy_cli import client
-
-username = ""
-password = ""
-image = "images/suntree.png"
-caption = "this is an auto-generated post"
-
-with client(username, password) as cli:
-    cli.upload(image, caption)
